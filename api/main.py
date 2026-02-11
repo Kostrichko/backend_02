60285fc8c8869c4de3da2c1164a6cf9bc925f348
Kostrichko <kostrichkoleg@gmail.com>
2026-02-11 19:52:15 +0700

    feat: CORS

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 import logging
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routers import task
 from shared import setup_logging
 
@@ -9,4 +10,13 @@ logger = logging.getLogger(__name__)
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(task.router, prefix="/tasks")
